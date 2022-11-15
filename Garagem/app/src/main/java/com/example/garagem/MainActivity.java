3ebf7b242a194fe630786c7ac99620e772083874
Samuel <samuel.jesus3@etec.sp.gov.br>
2022-11-15 19:16:26 -0300

    começo da parte logica da tela principal

--- Garagem/app/src/main/java/com/example/garagem/MainActivity.java
+++ Garagem/app/src/main/java/com/example/garagem/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.garagem;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder txtConfirmacao = new AlertDialog.Builder(MainActivity.this);
+        txtConfirmacao.setTitle("Confirmação");
+        txtConfirmacao.setMessage("Deseja reservar essa vaga?");
+
+
+        Button a1 = findViewById(R.id.btn_A1);
+        TextView tt = findViewById(R.id.textView1);
+
+        Object [][] vaga = new Object[12][4];
+        vaga[0][0] = a1.toString();
+        vaga[0][1] = "alam";
+        vaga[0][2] = "giriponga";
+        vaga[0][3] = false;
+
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                tt.setText(vaga[0][1].toString());
+                if(vaga[0][3].equals(false)){
+                txtConfirmacao.create().show();
+                vaga[0][3] = true;
+
+                }
+            }
+        });
     }
 }
